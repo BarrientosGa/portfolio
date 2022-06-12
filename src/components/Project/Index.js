e9ca80b5c5991ab09c934997ee7ee8ac427769c3
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-06-12 13:16:12 -0300

    se borro los datos mock y se agrego archivo json

--- src/components/Project/Index.js
+++ src/components/Project/Index.js
@@ -4,62 +4,9 @@ import { useStyles } from './styles/projectsStyles'
 import { motion } from "framer-motion"
 import CardProject from './Card/CardProject'
 import DividerComp from '../Share/Divider/DividerComp'
+import project from '../../json/project.json'
 
-const projects = [
-  {
-    id: 1,
-    img: './ongSomosMas.png',
-    title: 'ONG Somos Mas',
-    description: `Este proyecto tiene dos campañas bien marcadas como lo son la campaña escuela y la campaña juguetes. 
-                  Donde en toda la aplicación se trata de mostrar en las diferentes secciones los 
-                  diferentes trabajos que se realizan en dichas campañas.`,
-    linkRepo: 'https://github.com/PlazaFer/somosmas.git'
-  },
-  {
-    id: 2,
-    img: './menuDePlatos.png',
-    title: 'Challenge Alkemy | Menu de platos',
-    description: `En esta app podrás armar tu propio menu de comida. Donde solo podes agregar 4 platos, 2 veganos y 2 no veganos. 
-                  Ademas al ir agregando cada plato, se muestra en pantalla los datos del menu, asi como: Precio total, Promedio 
-                  total de preparación y El puntaje de salud.`,
-    linkRepo: 'https://github.com/BarrientosGa/challenger-alkemy.git'
-  },
-  {
-    id: 3,
-    img: './inceptia.png',
-    title: 'Challenge InceptIA',
-    description: `El proyecto tiene un login para los usuarios, donde al iniciar sesión el usuario va tener diferentes bots/clientes asociados. 
-                  A partir de eso puede buscar en un rango de fechas los casos administrados por ese bots.`,
-    linkRepo: 'https://github.com/BarrientosGa/devChallengeReact.git'
-  },
-  {
-    id: 4,
-    img: './calculadora.png',
-    title: 'Calculadora',
-    description: `Una app donde se puede hacer diferentes cálculos como, suma, resta, división, 
-                  sacar porcentaje etc.`,
-    linkRepo: 'https://github.com/BarrientosGa/Calculator-online.git'
-  },
-  {
-    id: 5,
-    img: './libros.png',
-    title: 'Buscador de libros',
-    description: `Una app donde podrás buscas libros de diferentes géneros, por autor , tema. 
-                  Ademas si te interesa algún libro podes leerlo directamente desde Google Books.`,
-    linkRepo: 'https://github.com/BarrientosGa/Book-finder.git'
-  },
-  {
-    id: 6,
-    img: './ecommerce.png',
-    title: 'E-commerce - Tienda de celulares',
-    description: `celulandia es una tienda online donde vas a poder simular la compra de productos, 
-                  eligiendo la cantidad, completando un formulario con tus datos y obteniendo el id de tu compra.`,
-    linkRepo: 'https://github.com/BarrientosGa/React-js.git'
-  },
-]
-
-
-const Project = ({current}) => {
+const Project = () => {
 
   const classes = useStyles()
   
@@ -69,7 +16,7 @@ const Project = ({current}) => {
       <DividerComp title='PROJECTOS' />
       <Container>
         <Grid container className={classes.gridContainer} spacing={3} justifyContent='center' rowGap={2}>
-          {projects.map(({ id, ...props }) => (
+          {project.map(({ id, ...props }) => (
             <Grid item key={id}>
               <motion.div whileHover={{ scale: 1.1 }}>
                 <CardProject {...props} />
--- src/components/Technology/Index.js
+++ src/components/Technology/Index.js
@@ -3,39 +3,7 @@ import { useStyles } from './styles/technologyStyles'
 import { Grid, Container, Box, Toolbar} from '@mui/material'
 import { motion } from "framer-motion"
 import DividerComp from '../Share/Divider/DividerComp'
-
-
-const logos = [
-  {
-    id: 1,
-    urlImg: './html_5.png'
-  },
-  {
-    id: 2,
-    urlImg: './css-3.png'
-  },
-  {
-    id: 3,
-    urlImg: './js.png',
-  },
-  {
-    id: 4,
-    urlImg: './react.png'
-  },
-  {
-    id: 5,
-    urlImg: './redux.png'
-  },
-  {
-    id: 6,
-    urlImg: './mui.png'
-  },
-  {
-    id: 7,
-    urlImg: './formik.png'
-  },
-
-]
+import iconsTechnology from '../../json/iconsTechnology.json'
 
 const Technology = () => {
 
@@ -47,7 +15,7 @@ const Technology = () => {
       <DividerComp title='TECNOLOGÍAS' />
       <Container sx={{ display: 'flex', justifyContent: 'center' }}>
         <Grid container spacing={3} lg={3} md={4} sm={2} className={classes.gridContainer} justifyContent='flex-start'>
-          {logos.map(({ id, urlImg }) => (
+          {iconsTechnology.map(({ id, urlImg }) => (
             <Grid item key={id} zeroMinWidth>
               <motion.img whileHover={{ scale: 1.2, transition: { duration: 1 }, rotate: 360 }} src={urlImg} className={classes.img} />
             </Grid>
