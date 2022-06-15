14cdcfc041a442c5d2b57010532f29c03345b20b
Gabriel Barrientos <gabrielbarrientos729@gmail.com>
2022-06-15 19:23:27 -0300

    se cambio el estilo de la seccion tecnologias

--- src/components/Technology/Index.js
+++ src/components/Technology/Index.js
@@ -1,28 +1,44 @@
 import React from 'react'
 import { useStyles } from './styles/technologyStyles'
-import { Grid, Container, Box, Toolbar } from '@mui/material'
+import { Grid, Container, Box, Toolbar, Paper, Typography } from '@mui/material'
 import { motion } from "framer-motion"
 import DividerComp from '../Share/Divider/DividerComp'
-import iconsTechnology from '../../json/iconsTechnology.json'
+import { technology } from '../../json/technology'
 
 const Technology = () => {
 
   const classes = useStyles()
 
   return (
-    <Box id='tecnologías'>
-      <Toolbar />
-      <DividerComp title='TECNOLOGÍAS' />
-      <Container className={classes.container}>
-        <Grid container spacing={3} className={classes.gridContainer} justifyContent='flex-start' >
-          {iconsTechnology.map(({ id, urlImg }) => (
-            <Grid item key={id} lg={4} md={4} sm={6} xs={12} zeroMinWidth>
-              <motion.img whileHover={{ scale: 1.2, transition: { duration: 1 }, rotate: 360 }} src={urlImg} className={classes.img} />
+    <>
+      <Box id='tecnologías'>
+        <Toolbar />
+        <DividerComp title='TECNOLOGÍAS' />
+      </Box>
+      <Container sx={{ width: '60%', marginTop: '40px' }}>
+        <Grid item container rowSpacing={3}>
+
+          {technology.map(({ id, text, svgIcon }) => (
+            <Grid key={id} item container xs={12} sm={12} md={6} lg={4} sx={{ justifyContent: 'center' }}>
+             {/*  <motion.div whileHover={{ border: '4px solid #3c0087', background: '#010312' }}> */}
+                <Paper sx={{ background: '#263238', width: '70%', padding: '8px', }}>
+
+                  <Grid item zeroMinWidth sx={{ display: 'flex', alignItems: 'center' }}>
+                    {svgIcon}
+                    <Typography variant="h5" color='secondary' fontWeight='bold' fontSize='1.25rem' marginLeft='10px' component='p'>
+                      {text}
+                    </Typography>
+                  </Grid>
+
+                </Paper>
+             {/*  </motion.div> */}
             </Grid>
+
           ))}
+
         </Grid>
       </Container>
-    </Box>
+    </>
   )
 }
 
---
+++ src/json/technology.js
@@ -0,0 +1,47 @@
+import IconHtml from '../components/Icons/IconHtml'
+import IconCss from '../components/Icons/IconCss'
+import IconJs from '../components/Icons/IconJs'
+import IconReact from '../components/Icons/IconReact'
+import IconRedux from '../components/Icons/IconRedux'
+import IconMui from '../components/Icons/IconMui'
+import IconsFormik from '../components/Icons/IconsFormik'
+
+export const technology =
+    [
+        {
+            id: 1,
+            text: 'HTML',
+            svgIcon: <IconHtml/>
+
+        },
+        {
+            id: 2,
+            text: 'CSS',
+            svgIcon: <IconCss/>
+        },
+        {
+            id: 3,
+            text: 'JavaScript',
+            svgIcon: <IconJs/>
+        },
+        {
+            id: 4,
+            text: 'React',
+            svgIcon: <IconReact/>
+        },
+        {
+            id: 5,
+            text: 'Redux',
+            svgIcon: <IconRedux/>
+        },
+        {
+            id: 6,
+            text: 'MUI',
+            svgIcon: <IconMui/>
+        },
+        {
+            id: 7,
+            text: 'Formik',
+            svgIcon: <IconsFormik/>
+        }
+    ]
